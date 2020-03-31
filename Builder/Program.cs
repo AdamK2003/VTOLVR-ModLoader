2d4134017505bed940aa5878359aa960d8a56aa5
Ben Wilson <benwilson2508@gmail.com>
2020-03-31 22:22:20 +0100

    added directory

--- Builder/Program.cs
+++ Builder/Program.cs
@@ -77,7 +77,7 @@ namespace Build
         {
             Log("Building VTOLVR-ModLoader.exe\n");
             Run(paths["nuget"],
-                "restore",
+                "restore " + dir + @"\VTOLVR-ModLoader",
                 @"\VTOLVR-ModLoader");
             Run(paths["msbuild"],
                 "VTOLVR-ModLoader.csproj -property:Configuration=Release;TargetFrameworkVersion=4.6 -tv:14.0",

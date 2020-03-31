fc920e38b29cd13e88d25025913fbca4998a368d
Ben Wilson <benwilson2508@gmail.com>
2020-03-31 17:29:41 +0100

    Renamed to builder

---
+++ Builder/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Build
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("HELLLLOOOOO");
+        }
+    }
+}

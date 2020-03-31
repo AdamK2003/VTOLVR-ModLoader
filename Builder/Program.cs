17725c9ced4a3b9859753d6486ec4c85bf802189
Ben Wilson <benwilson2508@gmail.com>
2020-03-31 18:12:52 +0100

    Updated builder

--- Builder/Program.cs
+++ Builder/Program.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.IO;
 
 namespace Build
 {
@@ -10,7 +11,7 @@ namespace Build
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("HELLLLOOOOO");
+            Console.WriteLine(Directory.GetCurrentDirectory());
         }
     }
 }

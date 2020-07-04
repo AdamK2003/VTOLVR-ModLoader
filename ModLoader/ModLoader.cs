d25a0c3293cd579de9e6dd3bd4628cae37273314
Shulin Adude <temperz871@gmail.com>
2020-07-04 13:55:46 -0400

    Made scenario's launched through modloader load, moved mods button.
    Removed LoadingSceneContorller.LoadScene(7) as it's redundant.
    Added VTScenario.LaunchScenario() to launch the scenario.
    Moved WaitForSeconds(5) to ready the player at the right itme.

--- ModLoader/ModLoader.cs
+++ ModLoader/ModLoader.cs
@@ -96,7 +96,7 @@ namespace ModLoader
             if (MainScreen == null)
                 LogError("Main Screen was null");
 
-            Log("Spawning Keyboards");
+            Log("Spawning Keyboards"); 
             stringKeyboard = Instantiate(assetBundle.LoadAsset<GameObject>("StringKeyboard")).GetComponent<VRKeyboard>();
             floatKeyboard = Instantiate(assetBundle.LoadAsset<GameObject>("FloatKeyboard")).GetComponent<VRKeyboard>();
             intKeyboard = Instantiate(assetBundle.LoadAsset<GameObject>("IntKeyboard")).GetComponent<VRKeyboard>();
@@ -107,7 +107,7 @@ namespace ModLoader
             Log("Creating Mods Button");//Mods Button
             GameObject SettingsButton = MainScreen.transform.GetChild(0).GetChild(0).GetChild(8).gameObject;
             GameObject ModsButton = Instantiate(assetBundle.LoadAsset<GameObject>("ModsButton"), SettingsButton.transform.parent);
-            ModsButton.transform.localPosition = new Vector3(-811,-112,0);
+            ModsButton.transform.localPosition = new Vector3(-811,-412,0);
             VRInteractable modsInteractable = ModsButton.GetComponent<VRInteractable>();
             modsInteractable.OnInteract.AddListener(delegate { OpenPage(Pages.Mods); SetDefaultText(); });
 

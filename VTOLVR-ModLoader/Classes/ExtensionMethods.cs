913d2db33c52c7f02cc13a121dfc2fb10ec42bb0
Ben Wilson <benwilson2508@gmail.com>
2020-10-20 10:28:45 +0100

    Removed extension method

--- VTOLVR-ModLoader/Classes/ExtensionMethods.cs
+++ VTOLVR-ModLoader/Classes/ExtensionMethods.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Newtonsoft.Json.Linq;
+using System;
 using System.Collections.Generic;
 using System.Drawing;
 using System.Drawing.Imaging;
@@ -36,23 +37,6 @@ namespace VTOLVR_ModLoader.Classes
                 }
             }
             return sb.ToString();
-        }
-        public static BitmapImage ToBitmapImage(this Bitmap bitmap)
-        {
-            using (var memory = new MemoryStream())
-            {
-                bitmap.Save(memory, ImageFormat.Png);
-                memory.Position = 0;
-
-                var bitmapImage = new BitmapImage();
-                bitmapImage.BeginInit();
-                bitmapImage.StreamSource = memory;
-                bitmapImage.CacheOption = BitmapCacheOption.OnLoad;
-                bitmapImage.EndInit();
-                bitmapImage.Freeze();
-
-                return bitmapImage;
-            }
         }
     }
 }

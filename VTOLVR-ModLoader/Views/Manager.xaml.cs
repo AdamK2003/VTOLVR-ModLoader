6d35120ee044ca36ad96a8979b97e157bb2c7575
Ben Wilson <benwilson2508@gmail.com>
2020-10-25 06:47:18 +0000

    Getting the website version of a mod

--- VTOLVR-ModLoader/Views/Manager.xaml.cs
+++ VTOLVR-ModLoader/Views/Manager.xaml.cs
@@ -1,18 +1,18 @@
-﻿using System;
+﻿using Newtonsoft.Json.Linq;
+using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net.Http;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
+using System.IO;
 using VTOLVR_ModLoader.Classes;
+using VTOLVR_ModLoader.Windows;
+using System.Net;
+using System.ComponentModel;
+using System.Diagnostics;
 
 namespace VTOLVR_ModLoader.Views
 {
@@ -43,6 +43,9 @@ namespace VTOLVR_ModLoader.Views
                 items = new List<Item>();
 
             List<BaseItem> downloadedMods = Helper.FindDownloadMods();
+            if (downloadedMods.Count > 0)
+                NoModsText.Visibility = Visibility.Hidden;
+
             for (int i = 0; i < downloadedMods.Count; i++)
             {
                 items.Add(new Item(
@@ -53,6 +56,12 @@ namespace VTOLVR_ModLoader.Views
                     false,
                     false,
                     downloadedMods[i].Directory.FullName));
+
+                if (downloadedMods[i].Json[ProjectManager.jID] != null)
+                {
+                    items[i].PublicID = downloadedMods[i].Json[ProjectManager.jID].ToString();
+                    RequestMod(downloadedMods[i].Json[ProjectManager.jID].ToString());
+                }
             }
         }
         private void FindSkins(ref List<Item> items)
@@ -76,6 +85,118 @@ namespace VTOLVR_ModLoader.Views
 
         }
 
+        private void RequestMod(string publicID)
+        {
+            HttpHelper.DownloadStringAsync(
+                $"{Program.url}{Program.apiURL}{Program.modsURL}/{publicID}",
+                RequestModsCallback);
+        }
+
+        private async void RequestModsCallback(HttpResponseMessage response)
+        {
+            Helper.SentryLog("Request Mods Callback", Helper.SentryLogCategory.Manager);
+            if (!response.IsSuccessStatusCode)
+            {
+                Notification.Show(response.StatusCode.ToString(), "Error");
+                return;
+            }
+
+            JObject json = Helper.JObjectTryParse(await response.Content.ReadAsStringAsync(), out Exception exception);
+            if (exception != null)
+            {
+                MessageBox.Show("There was an error");
+                return;
+            }
+
+            for (int i = 0; i < _mods.Count; i++)
+            {
+                if (!string.IsNullOrEmpty(_mods[i].PublicID) &&
+                    _mods[i].PublicID.Equals(json["pub_id"].ToString()))
+                {
+                    _mods[i].WebsiteVersion = json["version"].ToString();
+                    _mods[i].UpdateVisibility = _mods[i].IsUptodate() == true ? Visibility.Hidden : Visibility.Visible;
+                    modsList.ItemsSource = _mods.ToArray();
+                    return;
+                }
+            }
+        }
+
+        //User has pressed the update button for one of their mods
+        private void UpdateMod(object sender, RoutedEventArgs e)
+        {
+            Button button = (Button)sender;
+            Helper.SentryLog($"Update button pressed for {button.ToolTip}", Helper.SentryLogCategory.Manager);
+            HttpHelper.DownloadStringAsync(
+                $"{Program.url}{Program.apiURL}{Program.modsURL}/{button.ToolTip}",
+                UpdateModReceivedInfo);
+        }
+        // Getting the json string to find the download link of the mod which the user
+        // wants to update
+        private async void UpdateModReceivedInfo(HttpResponseMessage response)
+        {
+            Helper.SentryLog("Update Mod Received Info", Helper.SentryLogCategory.Manager);
+            if (!response.IsSuccessStatusCode)
+            {
+                Notification.Show(response.StatusCode.ToString(), "Error");
+                return;
+            }
+
+            JObject json = Helper.JObjectTryParse(await response.Content.ReadAsStringAsync(), out Exception exception);
+            if (exception != null)
+            {
+                MessageBox.Show("There was an error");
+                return;
+            }
+
+            if (json["user_uploaded_file"] != null)
+            {
+                string fileName = GetFileName(json["user_uploaded_file"].ToString());
+                Console.Log("Downloading " + fileName);
+                HttpHelper.DownloadFile(
+                    json["user_uploaded_file"].ToString(),
+                    $"{Program.root}{Program.modsFolder}\\{fileName}",
+                    ModDownloadProgress, ModDownloadComplete); ;
+                return;
+            }
+            Notification.Show("There seems to be no file for this mod on the website. Please contact vtolvr-mods.com staff saying which mod it is.", "Strange Error");
+        }
+
+        private void ModDownloadProgress(object sender, DownloadProgressChangedEventArgs e)
+        {
+            Console.Log($"[{e.ProgressPercentage}%] Downloading Mod Update");
+            //The download progress still does get called at 100%, so just added this check
+            //so that ModDownloadComplete gets the last call.
+            if (e.ProgressPercentage != 100)
+            {
+                //If the user downloads multiple updates at once, this progrress bar is going
+                //too look glitchy, jumping back and fouth.
+                MainWindow.SetProgress(e.ProgressPercentage, "Downloading Mod Updates");
+                MainWindow.SetBusy(true);
+            }
+
+        }
+        //The zip from the website has finished downloading and is now in their mods folder
+        private void ModDownloadComplete(object sender, AsyncCompletedEventArgs e)
+        {
+            Helper.SentryLog("Fininshed downloading mod update", Helper.SentryLogCategory.Manager);
+            MainWindow.SetBusy(false);
+
+            MessageBox.Show(sender.GetType().Name);
+            if (!e.Cancelled && e.Error == null)
+            {
+                MainWindow.SetProgress(100, $"Ready");
+                Console.Log("Finished downloading mod update");
+            }
+            else
+            {
+                //MainWindow.SetProgress(100, $"Ready");
+                //Notification.Show($"{e.Error.Message}", "Error when downloading file");
+                //Console.Log("Error:\n" + e.Error.ToString());
+                //if (File.Exists(Path.Combine(Program.root, currentDownloadFile)))
+                //    File.Delete(Path.Combine(Program.root, currentDownloadFile));
+            }
+        }
+
         public class Item
         {
             public string Name { get; set; }
@@ -85,6 +206,7 @@ namespace VTOLVR_ModLoader.Views
             public bool LoadOnStartCheck { get; set; }
             public bool AutoUpdateCheck { get; set; }
             public string FolderDirectory { get; set; }
+            public string PublicID { get; set; }
 
             public Item(string name, Visibility updateVisibility, string currentVersion, string websiteVersion, bool loadOnStartCheck, bool autoUpdateCheck, string folderDirectory)
             {
@@ -96,6 +218,21 @@ namespace VTOLVR_ModLoader.Views
                 AutoUpdateCheck = autoUpdateCheck;
                 FolderDirectory = folderDirectory;
             }
+            public bool IsUptodate()
+            {
+                return CurrentVersion.Equals(WebsiteVersion);
+            }
+        }
+
+        /// <summary>
+        /// Gets a file name from the end of the URL
+        /// </summary>
+        /// <param name="url"></param>
+        /// <returns></returns>
+        private static string GetFileName(string url)
+        {
+            string[] split = url.Split('/');
+            return split[split.Length - 1];
         }
     }
 }

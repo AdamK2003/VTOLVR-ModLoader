fef25d03e7c373d56ff1c90e72c43177478dcc9f
Ben <me@ben-w.com>
2021-08-23 12:07:23 +0100

    Fixed issue when users had another . in their games path

--- Launcher/Program.cs
+++ Launcher/Program.cs
@@ -269,7 +269,9 @@ namespace Launcher
             if (overideItemsToExtract)
                 _itemsToExtract = overideAmount;
 
-            string currentFolder = zipPath.Split('.')[0];
+
+            // Removing the .zip at the end of this path to create the folder
+            string currentFolder = zipPath.Remove(zipPath.Length - 4);
             Directory.CreateDirectory(currentFolder);
 
             ItemHandler handler = new ItemHandler();

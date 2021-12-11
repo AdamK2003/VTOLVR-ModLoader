b89e3c8e72906ed8220b6f476c9bcfe1d58d9e9c
Ben <me@ben-w.com>
2021-12-11 13:55:05 +0000

    Started on material window

---
+++ Launcher/Windows/MaterialWindow.xaml.cs
@@ -0,0 +1,12 @@
+﻿using System.Windows;
+
+namespace Launcher.Windows
+{
+    public partial class MaterialWindow : Window
+    {
+        public MaterialWindow()
+        {
+            InitializeComponent();
+        }
+    }
+}
